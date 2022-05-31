e2a35d086d0473e9326d22be9b79e688a51a4666
Sam Contapay <sam@azragames.com>
2022-05-31 13:02:53 -0600

    Getting this to work with React webpack < 5 is breaking all the things

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Wallet } from './Wallet'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App loggedInText='You are not logged in' />
+    <Wallet />
   </React.StrictMode>
 );
 
